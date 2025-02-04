fdeeeb1754ba4cf4d9bf79f647df00b8fe20cae6
Sanchez Villagomez <christian.sanchezvillagomez@capgemini.com>
2025-02-04 15:58:20 +0100

    feat: implement calculatePrice for test to pass testEmptyBasketReturnsZero

--- src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
+++ src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
@@ -5,7 +5,8 @@ import java.util.Map;
 public class BookPricingService {
 
 	public double calculatePrice(Map<String, Integer> basket) {
-
+		if(basket == null || basket.isEmpty())
+			return 0;
 		return -1;
 	}
 	

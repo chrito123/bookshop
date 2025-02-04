320f34edd5f28a5d2969598ceeeeec13329ec2ba
Sanchez Villagomez <christian.sanchezvillagomez@capgemini.com>
2025-02-04 16:08:31 +0100

    feat: implement calculatePrice for test to pass testBasketReturnsPriceForOneBook

--- src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
+++ src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
@@ -3,11 +3,16 @@ package com.sanchezc.bookshop.service;
 import java.util.Map;
 
 public class BookPricingService {
-
+	double unitPriceBook = 50 ;
+	
 	public double calculatePrice(Map<String, Integer> basket) {
 		if(basket == null || basket.isEmpty())
 			return 0;
-		return -1;
+		double price = 0d;
+		for (String  book : basket.keySet()) {
+			price += unitPriceBook ; 
+		}
+		return price;
 	}
 	
 }

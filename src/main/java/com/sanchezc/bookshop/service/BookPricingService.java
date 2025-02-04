b865fbefa57109018eaa00553d2be2a4dddd41e5
Sanchez Villagomez <christian.sanchezvillagomez@capgemini.com>
2025-02-04 19:49:38 +0100

    feat: implement calculatePrice for test to pass testBasketReturnsPriceForDifferentBooks

--- src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
+++ src/main/java/com/sanchezc/bookshop/service/BookPricingService.java
@@ -9,11 +9,21 @@ public class BookPricingService {
 		if (basket == null || basket.isEmpty())
 			return 0;
 		double price = 0d;
-
+		double discount = getDiscount(basket.keySet().size());
 		for (String book : basket.keySet()) {
-			price += unitPriceBook * basket.get(book);
+			price += unitPriceBook * basket.get(book) * (1 - discount);
 		}
 		return price;
+	}
+
+	private double getDiscount(int uniqueBooks) {
+		return switch (uniqueBooks) {
+		case 2 -> 0.05;
+		case 3 -> 0.10;
+		case 4 -> 0.20;
+		case 5 -> 0.25;
+		default -> 0d;
+		};
 	}
 
 }
